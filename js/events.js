1d8e8b267799fb23cf583232ea72dc740e2d98db
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-15 22:20:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt() {
 
 function pressIt(){
   $('#typing').on('keydown',function(e){
-    if (e.which === 71)){
+    if (e.which === 71){
       alert('You pressed the G key.')
     }
   })
