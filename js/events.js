54a5cc8732b9f47f044786d0ef1963c2120b9311
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-15 22:08:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt() {
 
 function pressIt(){
   $('#typing').on('keydown',function(e){
-    if (e.key == 'G'){
+    if (e.which == 'G'){
       alert('You motherfucker')
     }
   })
